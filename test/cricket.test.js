8319e859a571a1711ee748277e1f0e91269a6530
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-09-20 10:17:06 +0200

    first commit

---
+++ test/cricket.test.js
@@ -0,0 +1,11 @@
+let assert = require("assert");
+let cricketScore = require("../cricket.js");
+
+describe('The  function', function () {
+
+    it(' should be able to  ', function () {
+    
+
+        assert.equal(cricketScore.cricketScore());
+    })
+    });
