e2d9fb12b00e5cdd084ff60c5f32e2c33c82d500
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-09-20 14:59:03 +0200

    tests

--- cricket.js
+++ cricket.js
@@ -1,3 +1,51 @@
-module.exports = function SettingsBill() {
-    
+module.exports = function Cricket(oversmax) {
+    let score = 0
+    let wicket = 0
+    let perOver = 0;
+
+
+    function scoreTrack() {
+
+        let cScore = "1-2-3-4-6-w"
+        let Tscore = cScore.split(" ")
+
+        for (i = 0; i < Tscore.length; i++) {
+            let point = Tscore[i]
+            if (perOver.length === 6) {
+                perOver++
+            }
+            if(perOver.length === 36){
+                return oversmax
+            }
+            if (point !== 'w') {
+                score += Number(point)
+            }
+            if (point === 'w') {
+                wicket = wicket + Number(point)
+            }
+        }
+
+    }
+
+    function gameOver() {
+         oversmax = {
+          score,
+          wicket,
+          perOver   
+         }   
+        if(wicket >= 10){
+            return oversmax
+        }
+      
+
+    }
+
+
+
+    return {
+        scoreTrack,
+        gameOver
+
+    }
+
 }
--- test/cricket.test.js
+++ test/cricket.test.js
@@ -1,11 +1,36 @@
 let assert = require("assert");
-let cricketScore = require("../cricket.js");
+let Cricket = require("../cricket.js");
 
-describe('The  function', function () {
+describe('The cricket function', function () {
 
-    it(' should be able to  ', function () {
-    
+    it(' should be able to get the score and add it to the total score for the game ', function () {
+        let cricketScore = Cricket()
 
-        assert.equal(cricketScore.cricketScore());
+        cricketScore.scoreTrack('-2--2-')
+
+        assert.deepEqual(cricketScore.scoreTrack("4"));
+    })
+
+    it(' should be able to get the score and add it to the total score for the game ', function () {
+        let cricketScore = Cricket()
+
+       cricketScore.scoreTrack('----')
+
+        assert.deepEqual(cricketScore.scoreTrack(0));
+    })
+
+    it(' should be able to get the amount of wickets fallen ', function () {
+        let cricketScore = Cricket()
+
+        cricketScore.scoreTrack('-2-4-3-')
+
+        assert.deepEqual(cricketScore.scoreTrack(9));
+    })
+
+    it(' should be able to stop the score when wickets have reached 10', function () {
+        let cricketScore = Cricket()
+
+
+        assert.deepEqual(cricketScore.gameOver(10));
     })
     });
